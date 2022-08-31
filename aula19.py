f4460e39b4bc78446a31f842823ba40bcb84e292
Ezequiel Machado Ferreira <ezequielmmf18@gmail.com>
2022-08-31 17:37:09 -0300

    Update Aula 19

--- aula19.py
+++ aula19.py
@@ -1,2 +1,8 @@
 # Aula 19 - Dicionários
 
+# Para criar um dicionário uso as {}
+dados = {'nome': 'Pedro', 'idade': 25}
+
+# Adicionando um elemento no dicionário
+dados['sexo'] = 'M'
+del dados['idade']
