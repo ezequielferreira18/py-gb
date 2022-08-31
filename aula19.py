40f0198484d5040f3ab7eac0f7b092dd8dcbb611
Ezequiel Machado Ferreira <ezequielmmf18@gmail.com>
2022-08-31 17:41:52 -0300

    Update aula19.py

--- aula19.py
+++ aula19.py
@@ -6,3 +6,18 @@ dados = {'nome': 'Pedro', 'idade': 25}
 # Adicionando um elemento no dicionário
 dados['sexo'] = 'M'
 del dados['idade']
+
+print(dados.values())
+
+filme = {'titulo': 'Star Wars',
+         'ano': 1977,
+         'diretor': 'George Lucas'
+        }
+# Exibe o conteúdo de cada elemento do dicionário
+print(filme.values())
+
+# Exibe os índices dos elementos do dicionário
+print(filme.keys())
+
+# Exibe os valores e os índices do dicionário
+print(filme.items())
